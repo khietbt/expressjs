53647a5f34f667e25eff2a0f688950fb53628536
khietbt <khietbt@gmail.com>
2024-03-03 06:25:35 +0700

    Update config to run jest with ts-paths

---
+++ 06-unit-testing/01-jest/src/utils/conversionUtils/__tests__/toInteger.test.ts
@@ -0,0 +1,12 @@
+import { InvalidNumberConversionException } from '@src/exceptions';
+import { toInteger } from '../toInteger';
+
+describe('Running testsuite on toInteger', () => {
+  test('Testcase 1. A valid integer', () => {
+    expect(toInteger('1')).toBe(1);
+  });
+
+  test('Testcase 2. An invalid integer', () => {
+    expect(() => toInteger('An invalid integer')).toThrow(InvalidNumberConversionException);
+  });
+});
