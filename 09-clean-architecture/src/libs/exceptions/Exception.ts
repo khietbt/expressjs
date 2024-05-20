2ec33dab3cbfe503d164148bf4a03976b0e5e469
khietbt <khietbt@gmail.com>
2024-05-20 12:46:06 +0700

    Update Exception

--- 09-clean-architecture/src/libs/exceptions/Exception.ts
+++ 09-clean-architecture/src/libs/exceptions/Exception.ts
@@ -1,7 +1,11 @@
 import { type IException } from './IException';
+import { type ExceptionCode } from './types';
 
 export abstract class Exception extends Error implements IException {
-  public constructor(public readonly message: string) {
+  public constructor(
+    public readonly code: ExceptionCode,
+    message: string
+  ) {
     super(message);
   }
 }
